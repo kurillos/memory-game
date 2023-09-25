d4b905cfac1991d4a0b06c4305a27cb8184dac8d
Kurillos <bocage.cyril@gmail.com>
2023-09-25 17:50:49 +0200

    Ajout de la fonction de creation de carte

---
+++ script.js
@@ -0,0 +1,47 @@
+
+/* Tableau d'URL des cartes */
+
+const cards = [
+
+    'https://picsum.photos/id/237/100/100', 
+
+    'https://picsum.photos/id/238/100/100',
+
+    'https://picsum.photos/id/239/100/100',
+
+    'https://picsum.photos/id/240/100/100',
+
+    'https://picsum.photos/id/241/100/100',
+
+    'https://picsum.photos/id/242/100/100',
+
+    'https://picsum.photos/id/243/100/100',
+
+    'https://picsum.photos/id/244/100/100'
+
+  ];
+
+/* Fonction de création de carte dans la Div game-board */
+
+function createCard(CardUrl) {
+
+    const card = document.createElement('div');
+
+    card.classList.add('card');
+
+    card.dataset.value = CardUrl;
+
+  
+
+    const cardContent = document.createElement('img');
+
+    cardContent.classList.add('card-content');
+
+    cardContent.src = `${CardUrl}`;
+
+    card.appendChild(cardContent);
+
+    return card;
+
+  }
+
